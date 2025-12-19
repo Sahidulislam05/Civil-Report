28aefc2fca2dc0122df27786cbd21707887882f3
Sahidul Islam <sahidulislamcst@gmail.com>
2025-12-19 11:20:04 +0600

    Update AllIssues.jsx

--- src/pages/Issues/AllIssues.jsx
+++ src/pages/Issues/AllIssues.jsx
@@ -121,7 +121,7 @@ export default function AllIssues() {
             </div>
           ) : (
             <>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center items-center">
                 {data.issues.map((issue) => (
                   <AllIssueCard key={issue._id} issue={issue} />
                 ))}
